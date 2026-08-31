ce00691b07e9d657ecee10d215b0b3207d1293ce
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2026-08-30 17:59:04 -0700

    Fix import statement and initialize model
    Fix import statement and initialize model

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ device = "cuda"
 repo_id = "mistralai/Voxtral-Mini-3B-2507"
 
 processor = AutoProcessor.form_pre_trained(repo_id)
+model = VoxtralForConditionalGeneration, AutoProcessor
+import torch
+
+device
