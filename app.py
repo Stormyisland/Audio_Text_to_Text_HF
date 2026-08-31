8ced3ab0996994f05cfb487b8aaa6518e9b1ab10
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2026-08-30 18:05:12 -0700

    Enhance model initialization with dtype and device
    Update model loading to specify dtype and device map.

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from transformers import VoxtralForConditionalGeneration, AutoProsessor
 device = "cuda"
 repo_id = "mistralai/Voxtral-Mini-3B-2507"
 
-processor = AutoProcessor.from_pre_trained(repo_id)
-model = VoxtralForConditionalGeneration.from_pretrained(
+processor = AutoProcessor.from_pretrained(repo_id)
+model = VoxtralForConditionalGeneration.from_pretrained(repo_id, dtype=torch.bfloat16, device_map=device)
 
